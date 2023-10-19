de1e6c39e9ca27345f928b49c59a95a062dbb181
diemkhangNT <diemkhang1912@gmail.com>
2023-10-19 16:08:47 +0700

    add_file_AutoMapper 19/10/23

---
+++ QuanLyThueTro/Dto/ChucVuDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyThueTro.Dto
+{
+    public class ChucVuDto
+    {
+        [Key]
+        public string idChucVu { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string tenChucVu { get; set; }
+    }
+}
