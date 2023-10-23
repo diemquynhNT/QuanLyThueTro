be0b2796aa7ec5f683a7e5dcdd225f842c034ba7
diemkhangNT <diemkhang1912@gmail.com>
2023-10-23 16:42:01 +0700

    fe_index_CCT 23/10/23

---
+++ Client_QuanLythueTro/Controllers/ChuChoThueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client_QuanLythueTro.Controllers
+{
+    public class ChuChoThueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Client_QuanLythueTro/Program.cs
+++ Client_QuanLythueTro/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Login}/{id?}");
+    pattern: "{controller=ChuChoThue}/{action=Index}/{id?}");
 
 app.Run();
